52a99e81257beb3dc53f6e4931ff27fe66a8637e
Syaamil Faiq <syaamil1136@gmail.com>
2024-09-15 14:53:09 +0800

    update meta and footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <div className="fixed bottom-0">
       <div className="w-screen p-6 flex justify-center items-center gap-4">
+        <span className="text-sm">
+          API from{" "}
+          <Link href="https://vidsrc.domains" className="underline">
+            vidsrc
+          </Link>
+        </span>
         <span className="text-sm">
           &copy;&nbsp;&nbsp;Syaamil Fa&apos;iq &#x2022; 2024
         </span>
